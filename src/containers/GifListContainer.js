4f9ae406ba86d1596cb898a670ed68be362b55e9
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-08 08:11:24 +0000

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,21 +1,34 @@
 import React from 'react'
 
 export default class GifListContainer extends React.Component {
-  state = { gifs: [] };
+  state = {
+    search: '',
+    gifs: []
+   };
+
+  handleChange = (e) => {
+      this.setState({
+          search: e.target.value
+      });
+  }
+
+  handleSubmit = () => {
+     fetch(`http://api.giphy.com/v1/gifs/search?q=${this.state.search}&api_key=dc6zaTOxFJmzC&rating=g`)
+     .then(res=>res.json)
+     .then(data =>{
+         this.setState({ gifs: data.data.map(g=>g.images.original.url).slice(0, 3) });
+       }
+     );
+  }
 
   componentDidMount() {
-    fetch(`http://api.giphy.com/v1/gifs/search?q=${YOUR QUERY HERE}&api_key=dc6zaTOxFJmzC&rating=g`)
-    .then(res=>res.json)
-    .then(data =>{
-        this.setState({ gifs: data.map(g=>g.images.original.url) });
-      }
-    );
+    this.handleSubmit();
   }
 
   render() {
     return (<div>
               <GifList gifs={this.state.gifs} />
-              <GifSearch />
+              <GifSearch onSubmit={this.handleSubmit} handleChange={this.handleChange} search={this.state.search} />
             </div>
           );
   }
