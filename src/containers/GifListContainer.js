0d581560448509ece12c1e37ccf4b588b60f9b12
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-08 08:08:18 +0000

    Automatically backed up by Learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -9,16 +9,21 @@ export default class GifListContainer extends React.Component {
   handleChange = (e) => {
       this.setState({
           query: e.target.value
-      })
+      });
+  }
+
+  handleSubmit = (event) => {
+     event.preventDefault();
+     fetch(`http://api.giphy.com/v1/gifs/search?q=${this.state.search}&api_key=dc6zaTOxFJmzC&rating=g`)
+     .then(res=>res.json)
+     .then(data =>{
+         this.setState({ gifs: data.data.map(g=>g.images.original.url).slice(0, 3) });
+       }
+     );
   }
 
   componentDidMount() {
-    fetch(`http://api.giphy.com/v1/gifs/search?q=${this.state.search}&api_key=dc6zaTOxFJmzC&rating=g`)
-    .then(res=>res.json)
-    .then(data =>{
-        this.setState({ gifs: data.data.map(g=>g.images.original.url).slice(0, 3) });
-      }
-    );
+    
   }
 
   render() {
