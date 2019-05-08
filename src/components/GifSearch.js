1e57af44bd6bee6684b0da2a2edaa666486442be
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-08 08:15:09 +0000

    Done.

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 export default class GifSearch extends React.Component {
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit();
+  }
+
   render(){
     return (
-        <form onSubmit={this.props.onSubmit}>
+        <form onSubmit={(e)=>{this.handleSubmit}}>
             <input type='text' value={this.props.search} onChange={this.props.handleChange}/>
             <button type='submit' className='btn primary'>Search</button>
         </form>
-    )
+    );
   }
 
 }
